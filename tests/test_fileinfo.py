6f8e4076fd80d88476e578e318c4a7ad606c6c6d
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-09-15 08:22:54 +0100

    First version

--- tests/test_fileinfo.py
+++ tests/test_fileinfo.py
@@ -1,2 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from fileinfo.fileinfo import FileInfo
+
+
+def test_init():
+    filename = 'somefile.ext'
+    fi = FileInfo(filename)
+    assert fi.filename == filename
+
+
+def test_init_relative():
+    filename = 'somefile.ext'
+    relative_path = '../{}'.format(filename)
+    fi = FileInfo(relative_path)
+    assert fi.filename == filename
